d8b5ad80253151e6d63e6214810ba9408dcf51b9
Your Name <you@example.com>
2023-02-01 08:29:15 +0700

    detail

---
+++ src/main/java/com/blog_module4_c0922h1/service/BlogService.java
@@ -0,0 +1,34 @@
+package com.blog_module4_c0922h1.service;
+
+import com.blog_module4_c0922h1.model.Blog;
+import com.blog_module4_c0922h1.repository.IBlogRepo;
+import jdk.nashorn.internal.runtime.options.Option;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BlogService {
+    @Autowired
+    IBlogRepo iBlogRepo;
+
+    public List<Blog> getAll(){
+        return (List<Blog>) iBlogRepo.findAll();
+    }
+
+    public void save(Blog blog){
+        iBlogRepo.save(blog);
+    }
+
+    public void deleteById(int id){
+        iBlogRepo.deleteById(id);
+    }
+
+  public Blog findById(int id){
+        return iBlogRepo.findById(id).get();
+  }
+}
